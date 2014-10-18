2bf57d27b16227b909c597af115d4dac7995c037
Ziyuan Jiang <ziyuanjiang@live.com>
2014-10-18 16:36:41 +0800

    start to unitest db.c

--- unittest/db_test.c
+++ unittest/db_test.c
@@ -2,5 +2,32 @@
 int main()
 {
 	db_initializer();
-	if(db_signin("a","12345"))
+	//should return signed in
+	switch(db_signin("a","12345"))
+	{
+		case 0:
+			printf("signed in\n");
+		case 1:
+			printf("already exist\n");
+		case -1:
+			printf("error\n");
+	}
+	//should return already exist
+	switch(db_signin("a","12345"))
+	{
+		case 0:
+			printf("signed in\n");
+		case 1:
+			printf("already exist\n");
+		case -1:
+			printf("error\n");
+	}
+
+	switch(db_login(usrname, data))
+	{
+		case 0:
+			printf("loged in\n");
+		case 1:
+			printf("");
+	}
 }
